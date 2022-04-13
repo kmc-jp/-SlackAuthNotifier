8d880f386317b0827dd8d721280f904cb1376873
TKMAX777 <service@tk-max.com>
2022-04-14 00:39:17 +0900

    fix: regexp

--- main.go
+++ main.go
@@ -21,9 +21,10 @@ func main() {
 
 	var slackHook = slack_webhook.New(os.Getenv("SLACK_TOKEN"))
 
-	var accepted = regexp.MustCompile(`Accepted\s(password|publickey)\sfor\s(\S+)from\s(\S+)port\s(\S+)`)
-	var failed = regexp.MustCompile(`Failed\s(password|publickey)\sfor\s(\S+)from\s(\S+)port\s(\S+)`)
-	var failedInvalidUser = regexp.MustCompile(`Failed\s(password|publickey)\sfor\sinvalid\suser\s(\S+)from\s(\S+)port\s(\S+)`)
+	var accepted = regexp.MustCompile(`Accepted\s+(password|publickey)\s+for\s+(\S+)\s+from\s+(\S+)\s+port\s+(\S+)`)
+	//Accepted publickey for tkmax777 from 192.168.100.64 port 52396 s
+	var failed = regexp.MustCompile(`Failed\s+(password|publickey)\s+for\s(\S+)\s+from\s(\S+)\s+port\s+(\S+)`)
+	var failedInvalidUser = regexp.MustCompile(`Failed\s+(password|publickey)\s+for\s+invalid\s+user\s+(\S+)\s+from\s+(\S+)\s+port\s+(\S+)`)
 
 	fmt.Println("Start Auth Notify")
 
